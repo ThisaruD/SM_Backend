29f0fcc60f5871ec7355005e62994613c314fa4b
ThisaruD <thisarudil55@gmail.com>
2021-09-03 14:37:43 +0530

    implement user profile create,update, delete

--- config/db.js
+++ config/db.js
@@ -3,20 +3,20 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 
-const connectDB = async () =>{
-   try{
-       await mongoose.connect(db,{
-           useNewUrlParser:true,
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
 
-       });
-       console.log('MongoDB connected...')
-   } catch (err){
-    console.error(err.message);
+        });
+        console.log('MongoDB connected...')
+    } catch (err) {
+        console.error(err.message);
 
-    //exit process with failure
-       process.exit(1);
+        //exit process with failure
+        process.exit(1);
 
-   }
+    }
 }
 
 module.exports = connectDB;
